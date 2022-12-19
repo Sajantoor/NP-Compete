edcae0a5dab0f3b841f693a24c94ab8adebb61b1
Sajan Toor <sajantoor98@gmail.com>
2022-12-19 15:32:37 -0800

    Implement OAuth with GitHub

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import { oAuthCallbackGithub, oAuthWithGithub } from "./authentication";
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
-app.get('/', (_, res) => {
-    res.send('Hello World!');
+app.get("/", (_, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/login", (_, res) => {
+    oAuthWithGithub(res);
+});
+
+app.get("/callback", (req, res) => {
+    oAuthCallbackGithub(req, res);
 });
 
 app.listen(port, () => {
