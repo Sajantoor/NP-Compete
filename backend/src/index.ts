e86e9af0e43631f617ee0317caa66686827dca53
Sajan Toor <sajantoor98@gmail.com>
2022-12-21 14:05:46 -0800

    Initialize WebSocket Based Rooms

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
 import { oAuthCallbackGithub, oAuthWithGithub } from "./authentication";
+import { WebSocketServer } from 'ws';
+import { createServer } from 'http';
+import { sendMessageToRoom } from "./websocket";
 dotenv.config();
 
 const app = express();
 const port = 3000;
+const server = createServer(app);
+
+const webSocketServer = new WebSocketServer({ server });
+
+webSocketServer.on("connection", (webSocket, req) => {
+    const roomId = req.url;
+
+    if (roomId == null) {
+        webSocket.close();
+        return;
+    }
+
+    webSocket.roomId = roomId;
+    sendMessageToRoom(webSocketServer, webSocket, roomId, "A new user has joined the room");
+
+    // handle close event...
+    webSocket.on("close", () => {
+        sendMessageToRoom(webSocketServer, webSocket, roomId, "A user has left the room");
+    });
+
+    // handle message event...
+    webSocket.on("message", (data) => {
+        sendMessageToRoom(webSocketServer, webSocket, roomId, data);
+    });
+});
 
 app.get("/", (_, res) => {
     res.send("Hello World!");
@@ -18,6 +46,6 @@ app.get("/callback", (req, res) => {
     oAuthCallbackGithub(req, res);
 });
 
-app.listen(port, () => {
-    return console.log("Server is listening on " + port);
-});
+server.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
