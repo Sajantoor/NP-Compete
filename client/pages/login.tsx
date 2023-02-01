a4f8dc4ad44474090559a0a476aac922ccec1880
Sajan Toor <sajantoor98@gmail.com>
2023-01-31 18:04:47 -0800

    Init Chakra UI + Basic Login Page

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,3 +1,4 @@
+import { Button, Flex, Heading, Input } from "@chakra-ui/react";
 import Router from "next/router";
 import { SERVER_URL } from "../constants";
 
@@ -19,12 +20,14 @@ export default function Login() {
 
 
     return (
-        <>
-            <div>
-                <h1> Login </h1>
-                <button onClick={loginWithGithub}> Login with GitHub </button>
-            </div>
-        </>
+        <Flex height="100vh" alignItems="center" justifyContent="center">
+            <Flex direction="column" background="gray.800" p={20}>
+                <Heading textAlign="center" color="gray.100" mb={6}> Login </Heading>
+                <Input type="email" color="gray.100" placeholder="Email" mb={6} />
+                <Input type="password" color="gray.100" placeholder="Password" mb={6} />
+                <Button variant="outline" background="gray.300" onClick={loginWithGithub}> Login with GitHub </Button>
+            </Flex>
+        </Flex>
     )
 }
 
