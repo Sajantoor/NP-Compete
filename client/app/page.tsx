cd938369340458a522affca745bce8586e2d62ed
Sajan Toor <sajantoor98@gmail.com>
2024-05-06 18:04:09 -0700

    Update home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,19 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Button, Card, Heading, Text } from "@chakra-ui/react";
 import NavBar from "./components/navBar";
 
 export default function Home() {
     return (
         <>
             <NavBar />
-            <Text fontSize="4xl">Hello World!</Text>
+            <Box padding={5}>
+                <Heading fontSize="4xl" padding={1}>
+                    NP Compete
+                </Heading>
+                <Text fontSize="l" padding={1}>
+                    A platform for <em>competing</em> against others in
+                    programming challenges.
+                </Text>
+            </Box>
         </>
     );
 }
